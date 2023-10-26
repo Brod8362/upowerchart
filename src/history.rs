44ccb569a24bcd9266f904ade16a6d33a87be528
Brod8362 <brod8362@gmail.com>
2023-10-26 11:17:18 -0500

    WIP history parsing

---
+++ src/history.rs
@@ -0,0 +1,31 @@
+use std::{path::Path, error::Error};
+
+pub struct HistoryEntry {
+    time: u64,
+    value: f64,
+    charging: bool
+}
+
+pub fn parse_file(path: &Path) -> Result<Vec<HistoryEntry>, ()> {
+    let file = std::fs::read_to_string(path).unwrap();
+    Ok(
+        file.lines().map(|l| {
+            let mut groups = l.split('\t');
+            HistoryEntry {
+                time: groups.next().unwrap().parse().unwrap(),
+                value: groups.next().unwrap().parse().unwrap(),
+                charging: groups.next().unwrap() == "discharging"
+            }
+        }).collect()
+    )
+}
+
+pub fn get_history(device_name: String, len: usize) -> Result<(Vec<HistoryEntry>, Vec<HistoryEntry>), ()> {
+    let charge = parse_file(Path::new("/var/lib/upower/history-charge-45N1029-86-18880.dat"))?;
+    let rate = parse_file(Path::new("/var/lib/upower/history-rate-45N1029-86-18880.dat"))?;
+    Ok((charge, rate))
+}
+
+pub fn generate_graph(charge: &[HistoryEntry], power: &[HistoryEntry]) {
+    todo!()
+}
