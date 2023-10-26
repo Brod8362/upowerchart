d314744c17c0bc0a3579a36788a8dc56ea5d3de0
Brod8362 <brod8362@gmail.com>
2023-10-26 12:31:08 -0500

    kind of working battery graph

--- src/history.rs
+++ src/history.rs
@@ -55,10 +55,44 @@ pub fn get_history(device_name: String, len: usize) -> Result<(Vec<HistoryEntry>
 }
 
 pub fn generate_graph(charge: &[HistoryEntry], power: &[HistoryEntry]) -> Result<ImageSurface, Box<dyn Error>> {
-    let surface = ImageSurface::create(cairo::Format::ARgb32, 100, 100)?;
+    let width = 600;
+    let height = 400;
+    let surface = ImageSurface::create(cairo::Format::ARgb32, width, height)?;
     let context = Context::new(&surface)?;
-    context.set_source_rgb(1.0, 0.0, 0.0);
+
+    //TODO these should be passed in via some kind of config object
+    let background_color = (0.0, 0.0, 0.0);
+    let battery_color = (0.0, 1.0, 0.0);
+
+    // fill background
+    context.set_source_rgb(background_color.0, background_color.1, background_color.2);
     context.paint()?;
+
+    
+    let max_power = power
+        .into_iter()
+        .map(|k| k.value)
+        .max_by(|x, y| x.total_cmp(y))
+        .unwrap();
+
+    //draw battery percentage 
+    {
+        let max_charge = 100.0f64;
+        context.set_source_rgb(battery_color.0, battery_color.1, battery_color.2);
+        let x_step = width as f64/charge.len() as f64;
+        for i in 1..charge.len() {
+            let prev = &charge[i-1];
+            let this = &charge[i];
+            let prev_x = (i-1) as f64*x_step;
+            let x = i as f64*x_step;
+            let prev_y = prev.value/max_charge;
+            let y = height as f64*(this.value/max_charge);
+            context.move_to(prev_x, height as f64 - prev_y);
+            context.line_to(x, height as f64 - y);
+        }
+        context.stroke()?;
+    }
+    
     
     Ok(surface)
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let (charge, power) = history::get_history(
         String::from("45N1029"), 100
     )?;
-    println!("{:?} {:?}", charge.last(), power.last());
+    // println!("{:?} {:?}", charge.last(), power.last());
 
     let graph = history::generate_graph(&charge, &power)?;
     
