459408a869b6aa7c9dc003cbc8a095cadd9ce891
Brod8362 <brod8362@gmail.com>
2023-10-26 15:31:14 -0500

    popup window instead of output image

--- src/history.rs
+++ src/history.rs
@@ -1,6 +1,6 @@
 use std::{path::Path, error::Error, cmp::max, borrow::{BorrowMut, Borrow}};
 
-use minifb::{Window, WindowOptions, Key};
+use minifb::{Window, WindowOptions, Key, MouseButton};
 use plotters::{prelude::{BitMapBackend, IntoDrawingArea, ChartBuilder, LabelAreaPosition}, series::{LineSeries, AreaSeries}, style::{full_palette::{ORANGE}, RGBColor, TextStyle, IntoTextStyle, Color, CYAN}, backend::BGRXPixel};
 
 use crate::buffer::BufferWrapper;
@@ -109,89 +109,93 @@ pub fn generate_graph(charge: &[HistoryEntry], power: &[HistoryEntry], model: &S
 
     // drawing 
     let mut buf = BufferWrapper(vec![0u32; width as usize*height as usize]);
-    let drawing_area = BitMapBackend::<BGRXPixel>::with_buffer_and_format(
-        buf.borrow_mut(), (width as u32, height as u32)
-    )?.into_drawing_area();
-    drawing_area.fill(&background_color)?;
+    let mut window = Window::new("battery", width as usize, height as usize, WindowOptions::default())?;
+    window.limit_update_rate(Some(std::time::Duration::from_millis(100)));
+    
+    {
+        let drawing_area = BitMapBackend::<BGRXPixel>::with_buffer_and_format(
+            buf.borrow_mut(), (width as u32, height as u32)
+        )?.into_drawing_area();
+        drawing_area.fill(&background_color)?;        
 
-    let window = Window::new("battery", width as usize, height as usize, WindowOptions::default())?;
+        let text_style = TextStyle::from(font)
+            .color(&axis_color)
+            .into_text_style(&drawing_area);
 
-    let text_style = TextStyle::from(font)
-        .color(&axis_color)
-        .into_text_style(&drawing_area);
+        let time_range = first_timestamp as i32..last_timestamp as i32;
 
-    let time_range = first_timestamp as i32..last_timestamp as i32;
+        let mut charge_chart = ChartBuilder::on(&drawing_area)
+            .margin(graph_margin)
+            .margin_bottom(graph_margin+bottom_margin_extra)
+            .set_label_area_size(LabelAreaPosition::Left, label_area_size)
+            .set_label_area_size(LabelAreaPosition::Bottom, label_area_size)
+            .build_cartesian_2d(time_range.clone(), 0..100)
+            .unwrap();
 
-    let mut charge_chart = ChartBuilder::on(&drawing_area)
-        .margin(graph_margin)
-        .margin_bottom(graph_margin+bottom_margin_extra)
-        .set_label_area_size(LabelAreaPosition::Left, label_area_size)
-        .set_label_area_size(LabelAreaPosition::Bottom, label_area_size)
-        .build_cartesian_2d(time_range.clone(), 0..100)
-        .unwrap();
+        let rate_max = power.iter().map(|x| x.value).max_by(|x, y| x.total_cmp(y)).unwrap().ceil() as i32;
 
-    let rate_max = power.iter().map(|x| x.value).max_by(|x, y| x.total_cmp(y)).unwrap().ceil() as i32;
+        let mut rate_chart = ChartBuilder::on(&drawing_area)
+            .margin(graph_margin)
+            .margin_bottom(graph_margin+bottom_margin_extra)
+            .set_label_area_size(LabelAreaPosition::Left, label_area_size)
+            .set_label_area_size(LabelAreaPosition::Bottom, label_area_size)
+            .build_cartesian_2d(time_range, 0..rate_max)
+            .unwrap();
 
-    let mut rate_chart = ChartBuilder::on(&drawing_area)
-        .margin(graph_margin)
-        .margin_bottom(graph_margin+bottom_margin_extra)
-        .set_label_area_size(LabelAreaPosition::Left, label_area_size)
-        .set_label_area_size(LabelAreaPosition::Bottom, label_area_size)
-        .build_cartesian_2d(time_range, 0..rate_max)
-        .unwrap();
+        // this is only used to draw a custom axis
+        let mut time_chart = ChartBuilder::on(&drawing_area)
+            .margin(graph_margin)
+            .margin_bottom(graph_margin+bottom_margin_extra)
+            .set_label_area_size(LabelAreaPosition::Left, label_area_size)
+            .set_label_area_size(LabelAreaPosition::Bottom, label_area_size)
+            .build_cartesian_2d(-hours..0, 0..100)
+            .unwrap();
 
-    // this is only used to draw a custom axis
-    let mut time_chart = ChartBuilder::on(&drawing_area)
-        .margin(graph_margin)
-        .margin_bottom(graph_margin+bottom_margin_extra)
-        .set_label_area_size(LabelAreaPosition::Left, label_area_size)
-        .set_label_area_size(LabelAreaPosition::Bottom, label_area_size)
-        .build_cartesian_2d(-hours..0, 0..100)
-        .unwrap();
+        //draws X axis with the marks being hours
+        //draws Y axis with 10 marks (being for battery %)
+        time_chart
+            .configure_mesh()
+            .disable_mesh()
+            .axis_style(&axis_color)
+            .x_desc("hours")
+            .x_labels(hours as usize)
+            .label_style(text_style.clone())
+            .draw()?;
 
-    //draws X axis with the marks being hours
-    //draws Y axis with 10 marks (being for battery %)
-    time_chart
-        .configure_mesh()
-        .disable_mesh()
-        .axis_style(&axis_color)
-        .x_desc("hours")
-        .x_labels(hours as usize)
-        .label_style(text_style.clone())
-        .draw()?;
+        //TODO: draw right Y axis for power
 
-    //TODO: draw right Y axis for power
+        //draw the actual data
+        charge_chart.draw_series(
+            AreaSeries::new(
+                charge_pct_series, 
+                0, 
+                percent_color.mix(0.2)
+            ).border_style(&percent_color)
+        )?;
 
-    //draw the actual data
-    charge_chart.draw_series(
-        AreaSeries::new(
-            charge_pct_series, 
-            0, 
-            percent_color.mix(0.2)
-        ).border_style(&percent_color)
-    )?;
+        let texts = [
+            (model.clone(), &text_style),
+            (format!("BAT: {}%", charge.last().unwrap().value), &text_style.color(&percent_color)),
+            (format!("PWR: {:.1}W", power.last().unwrap().value), &text_style.color(&discharging_color))
+        ];
 
-    let texts = [
-        (model.clone(), &text_style),
-        (format!("BAT: {}%", charge.last().unwrap().value), &text_style.color(&percent_color)),
-        (format!("PWR: {:.1}W", power.last().unwrap().value), &text_style.color(&discharging_color))
-    ];
+        let (_, text_h) = drawing_area.estimate_text_size("?", &text_style)?;
+        let y_pos = (height - text_h as i32- bottom_margin_extra/2) as i32;
+        let mut x = 20;
+        for (text, style) in texts {
+            drawing_area.draw_text(&text, &style, (x, y_pos))?;
+            let (tx, _) = drawing_area.estimate_text_size(&text, &style)?;
+            x = x + tx as i32 + 5; //5 is text margin
+        }
+        
+        rate_chart.draw_series(LineSeries::new(chg_rate_series, charging_color))?;
+        rate_chart.draw_series(LineSeries::new(dischchg_rate_series, discharging_color))?;
 
-    let (_, text_h) = drawing_area.estimate_text_size("?", &text_style)?;
-    let y_pos = (height - text_h as i32- bottom_margin_extra/2) as i32;
-    let mut x = 20;
-    for (text, style) in texts {
-        drawing_area.draw_text(&text, &style, (x, y_pos))?;
-        let (tx, _) = drawing_area.estimate_text_size(&text, &style)?;
-        x = x + tx as i32 + 5; //5 is text margin
+        drawing_area.present()?;
     }
     
-    rate_chart.draw_series(LineSeries::new(chg_rate_series, charging_color))?;
-    rate_chart.draw_series(LineSeries::new(dischchg_rate_series, discharging_color))?;
-
-    drawing_area.present()?;
-
-
-    window.update_with_buffer(buf.borrow(), width as usize, height as usize)?;
+    while window.is_open() && !(window.get_mouse_down(MouseButton::Left) || window.is_key_down(Key::Escape)) {
+        window.update_with_buffer(buf.borrow(), width as usize, height as usize)?;
+    }
     Ok(())
 }
